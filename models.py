dffc6f06baaf0dae85c069f829f89295cf065f00
Oladeji24 <51147155+Oladeji24@users.noreply.github.com>
2021-12-22 08:45:43 +0100

    Add files via upload

---
+++ models.py
@@ -0,0 +1,21 @@
+import datetime
+from django.utils import timezone
+from django.db import models
+from django.db.models.deletion import CASCADE
+
+class Question(models.Model):
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+            return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    def __str__(self):
+        return self.choice_text
+
+        question = models.ForeignKey(Question, on_delete=models.CASCADE)
+        choice_text = models.CharField(max_length=200)
+        votes = models.IntegerField(default=0)
+
+        
